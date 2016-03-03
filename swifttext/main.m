57ed3a7c267f4ffd71c74a480835011051cb1750
20131105798 <a20131105798@admindeimac-12.local>
2016-03-03 11:35:08 +0800

    Initial Commit

---
+++ swifttext/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  swifttext
+//
+//  Created by 20131105798 on 16/3/3.
+//  Copyright © 2016年 20131105798. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
